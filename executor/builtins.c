0593a130dc0b14bba2691eec631fdb5514698532
ricardomart26 <ricardomartins2626t@gmail.com>
2021-11-11 16:10:30 +0000

    builtins_update

--- executor/builtins.c
+++ executor/builtins.c
@@ -6,7 +6,7 @@
 /*   By: rimartin <rimartin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/20 10:05:27 by jmendes           #+#    #+#             */
-/*   Updated: 2021/11/10 21:57:17 by rimartin         ###   ########.fr       */
+/*   Updated: 2021/11/11 16:10:00 by jmendes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -102,8 +102,99 @@ t_env	*singleton_env(t_env *link) // Design pattern
 	return (new_link);
 }
 
+void list_sort(t_lista *lst)
+{
+	t_lista *current;
+	t_lista *after;
+	char *temp;
+	int index;
 
-int	builtins(t_parse *st, t_env **env, t_node **node)
+	index = 0;
+	current = lst;
+	while (current->next != NULL)
+	{
+		after = current->next;
+		while (after != NULL)
+		{
+			index = 0;
+			if ((unsigned char)current->content[0] > (unsigned char)after->content[0])
+			{
+				temp = ft_strdup(current->content);
+				current->content = ft_strdup(after->content);
+				after->content = ft_strdup(temp);
+			}
+			else if ((unsigned char)current->content[0] == (unsigned char)after->content[0])
+			{
+				while ((unsigned char)current->content[index] == (unsigned char)after->content[index])
+				{
+					index++;
+					if ((unsigned char)current->content[index] > (unsigned char)after->content[index])
+					{
+						temp = ft_strdup(current->content);
+						current->content = ft_strdup(after->content);
+						after->content = ft_strdup(temp);
+					}
+					else if ((unsigned char)current->content[index] < (unsigned char)after->content[index])
+						break;
+				}
+			}
+	
+			after = after->next;
+		}
+		printf("declare -x %s\n", current->content);
+		current = current->next;
+	}
+	printf("declare -x %s\n", current->content);
+	//deallocate(lst);
+	//free(temp);
+	//deallocate(&current);
+}
+
+int export(char *var, t_lista *envp, t_lista *sort)
+{
+	if (var == NULL)
+		list_sort(sort);
+	else
+	{
+		if (copy_check(var, sort, envp) == 0)
+		{
+			ft_lstadd_back((void *)&sort, ft_lstnew((void *)ft_strdup(var)));
+			if (char_check(var, '=') != -1)
+				ft_lstadd_back((void *)&envp, ft_lstnew((void *)ft_strdup(var)));
+		}
+	}
+	return (0);
+}
+
+int	env(t_lista *lst)
+{
+	t_lista *current;
+
+	current = lst;
+	while (current != NULL)
+	{
+		printf("%s\n", (char *)current->content);
+		current = current->next;
+	}
+	return (0);
+}
+
+int 	list_init(t_listas *s_listas, char **envp)
+{
+	int index;
+
+	index = 0;
+	lst_envp = NULL;
+	while (envp[index] != NULL)
+	{
+		ft_lstadd_back((void *)&s_listas->envp, ft_lstnew((void *)ft_strdup(envp[index])));
+		ft_lstadd_back((void *)&s_listas->sort, ft_lstnew((void *)ft_strdup(envp[index])));
+		index++;
+	}
+	return (0);
+}
+
+int	builtins(t_parse *st, t_env **env, t_node **node, t_listas *s_listas, char **envp)
 {
 	int		echo_n;
 	int		error_code;
@@ -111,6 +202,7 @@ int	builtins(t_parse *st, t_env **env, t_node **node)
 
 	echo_n = 0;
 	error_code = 3;
+	list_init(s_listas, envp);
 	line = ft_split_quotes((*node)->cmd, ' ');
 	if (ft_strncmp(line[0], "cd", ft_strlen(line[0])) == 0)
 		error_code = cd(line[1]);
@@ -124,5 +216,9 @@ int	builtins(t_parse *st, t_env **env, t_node **node)
 	}
 	if (ft_strncmp(line[0], "exit", ft_strlen(line[0])) == 0)
 		exit_builtin(st, env, node);
+	if (ft_strncmp(line[0], "export",ft_strlen(line[0])) == 0)
+		export(line[1], t_listas->envp, t_listas->sort);
+	if (ft_strncmp(line[0], "env",ft_strlen(line[0])) == 0)
+		export(t_listas->envp);
 	return (0);
 }
