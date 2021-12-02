158cdfcb5aa8aeba717de882c56f379268b3a8f2
ricardomart26 <ricardomartins2626t@gmail.com>
2021-12-02 20:43:35 +0000

    norm

--- environment/env.c
+++ environment/env.c
@@ -6,156 +6,38 @@
 /*   By: rimartin <rimartin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/08 12:58:47 by rimartin          #+#    #+#             */
-/*   Updated: 2021/11/17 23:21:44 by rimartin         ###   ########.fr       */
+/*   Updated: 2021/12/02 20:21:41 by jmendes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-// /**
-//  * 
-//  * @define: Retorna o size da env variable  
-//  *  
-//  * @params: 
-//  * 
-//  * @return_value:
-//  * 
-//  */
+void	ft_export(char *var, t_lista *envp, t_lista *sort)
+{
+	if (var == NULL)
+		list_sort(sort);
+	else
+	{
+		if (copy_check(var, sort, envp) == 0)
+		{
+			ft_lstadd_back((void *)&sort, ft_lstnew((void *)ft_strdup(var)));
+			if (char_check(var, '=') != -1)
+				ft_lstadd_back((void *)&envp,
+					ft_lstnew((void *)ft_strdup(var)));
+		}
+	}
+}
 
-// char	*cut_string(char *cmd, int start, int end)
-// {
-// 	char	*ret;
-// 	int		i;
-// 	int		size;
+void	ft_env(t_lista *lst, char *is_not_null)
+{
+	t_lista	*current;
 
-// 	if (start >= end || end > ft_strlen(cmd))
-// 		exit(4);
-// 	size = end - start;
-// 	ret = malloc(size + 1);
-// 	i = -1;
-// 	while (++i < size)
-// 		ret[i] = cmd[start + i];
-// 	ret[i] = '\0';
-// 	return (ret);
-// }
-
-// char	*rem_from_str(char *cmd, int start, int len)
-// {
-// 	char	*ret;
-// 	int		i;
-// 	int		x;
-
-// 	if (start >= ft_strlen(cmd))
-// 		exit(4);
-// 	if (start == 0)
-// 		return (ft_strdup(cmd + len));
-// 	if (len + start >= ft_strlen(cmd))
-// 		return (ft_substr(cmd, start, len));
-// 	ret = malloc(len + 1);
-// 	i = -1;
-// 	while (++i < start)
-// 		ret[i] = cmd[i];
-// 	x = -1;
-// 	while (cmd[start + len] != '\0')
-// 	{
-// 		ret[i] = cmd[start + len + i];
-// 		i++;
-// 	}
-// 	ret[i] = '\0';
-// 	return (ret);
-// }
-
-// /**
-//  * 
-//  * @definition:
-//  * 
-//  * @params:
-//  * 
-//  * @return_value:
-//  * 
-//  */
-
-// int	size_rem_env(char *line, int start, bool brackets, int *i)
-// {
-// 	int	size;
-
-// 	*i = set_brackets_to_true(line, (*i) + 1, &brackets);
-// 	if (brackets == true)
-// 	{
-// 		size = 2;
-// 		while (line[start++] != '}')
-// 			size++;
-// 		return (size + 1);
-// 	}
-// 	size = 1;
-// 	while (line[start] && !is_space(line[start]) && size++)
-// 		start++;
-// 	return (size - 1);
-// }
-
-// /**
-//  * 
-//  * @definition: Replace the env variable in the line to the
-//  * actual value 
-//  * 
-//  * @params: line -> command line
-//  * 
-//  * @return_value: line with the environment variables values
-//  * 
-//  */
-
-// char	*add_value(char *line, int start, char *value)
-// {
-// 	int		size;
-// 	char	*ret;
-// 	int		i;
-// 	int		x;
-
-// 	size = ft_strlen(line) + ft_strlen(value);
-// 	ret = malloc(size + 1);
-// 	i = -1;
-// 	while (++i < start)
-// 		ret[i] = line[i];
-// 	i = -1;
-// 	while (value[++i])
-// 		ret[start + i] = value[i];
-// 	if (start + i == size)
-// 	{
-// 		ret[start + i] = '\0';
-// 		return (ret);
-// 	}
-// 	x = -1;
-// 	while (line[++x] != '\0')
-// 		ret[start + i++] = line[x];
-// 	ret[start + i] = '\0';
-// 	free(line);
-// 	return (ret);
-// }
-
-// void	expand_vars(char *line, int pos, int i, bool bracket)
-// {
-// 	char	*name;
-// 	char	*value;
-// 	int		start;
-// 	int		end;
-
-// 	start = 0;
-// 	while (line[++i])
-// 	{
-// 		if (line[i] == '$')
-// 		{
-// 			pos = i + 1;
-// 			name = ft_substr(line, pos, size_rem_env(line, pos, bracket, &i));
-// 			value = getenv(name);
-// 			if (value == NULL)
-// 				printf("Didn't found env %s\n", name);
-// 			end = pos + ft_strlen(name);
-// 			pos--;
-// 			line = add_value(rem_from_str(line, pos, end), pos, value);
-// 			i += ft_strlen(value) - 1;
-// 			if (i >= ft_strlen(line))
-// 				break ;
-// 			start = i;
-// 		}
-// 	}
-// }
+	if (is_not_null)
+		g_gl.error_code = COMMAND_NOT_FOUND;
+	current = lst;
+	while (current != NULL && !g_gl.error_code)
+	{
+		printf("%s\n", (char *)current->content);
+		current = current->next;
+	}
+}
--- environment/unset.c
+++ environment/unset.c
@@ -6,7 +6,7 @@
 /*   By: rimartin <rimartin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/13 16:08:18 by rimartin          #+#    #+#             */
-/*   Updated: 2021/12/02 19:35:43 by jmendes          ###   ########.fr       */
+/*   Updated: 2021/12/02 20:40:18 by jmendes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,18 +38,17 @@ t_lista	*unset1(char *path, t_lista *lst)
 
 	index = 0;
 	current = lst;
-		printf("Removeu o primeiro?\n");
 	if (first_unset(path, &lst) == 0)
 		return (lst);
 	while (current->next != NULL)
 	{
 		index = char_check(current->next->content, '=');
-		if (!ft_strncmp(current->next->content, path, ft_strlen(path)) || (index == -1
+		if ((ft_strlen(path) == index && !ft_strncmp(current->next->content,
+					path, ft_strlen(path))) || (index == -1
 				&& !ft_strncmp(current->next->content, path, ft_strlen(path))))
 		{
 			to_remove = current->next;
 			current->next = current->next->next;
-		printf("Removeu o primeiro? %s\n", to_remove->content);
 			free(to_remove);
 			to_remove = NULL;
 			return (lst);
@@ -59,65 +58,44 @@ t_lista	*unset1(char *path, t_lista *lst)
 	return (lst);
 }
 
+t_lista	*sub_unset(t_lista *current, t_listas *listas, char *path)
+{
+	int		index;
+	t_lista	*to_remove;
+
+	index = 0;
+	index = char_check(current->next->content, '=');
+	if ((ft_strlen(path) == index && !ft_strncmp(current->next->content,
+				path, ft_strlen(path))) || (index == -1
+			&& !ft_strncmp(current->next->content, path, ft_strlen(path))))
+	{
+		to_remove = current->next;
+		current->next = current->next->next;
+		printf("%s\n", to_remove->content);
+		free(to_remove);
+		to_remove = NULL;
+		listas->sort = unset1(path, listas->sort);
+		return (listas->sort);
+	}
+	return (listas->sort);
+}
+
 t_listas	*ft_unset(char *path, t_listas *listas)
 {
 	t_lista	*current;
-	t_lista	*to_remove;
-	int		index;
 
-	index = 0;
 	current = listas->linked_env;
 	if (first_unset(path, &listas->linked_env) == 0)
 	{
 		listas->sort = unset1(path, listas->sort);
 		return (listas);
 	}
-	while (current->next != NULL)
+	while (current != NULL)
 	{
-		index = char_check(current->next->content, '=');
-		if (!ft_strncmp(current->next->content, path, ft_strlen(path)) || (index == -1
-				&& !ft_strncmp(current->next->content, path, ft_strlen(path))))
-		{
-			to_remove = current->next;
-			current->next = current->next->next;
-			free(to_remove);
-			to_remove = NULL;
-			listas->sort = unset1(path, listas->sort);
-			return (0);
-		}
+		listas->sort = sub_unset(current, listas, path);
 		current = current->next;
 	}
 	if (unset1(path, listas->sort) == 0)
 		return (listas);
 	return (listas);
-}
-
-void	ft_export(char *var, t_lista *envp, t_lista *sort)
-{
-	if (var == NULL)
-		list_sort(sort);
-	else
-	{
-		if (copy_check(var, sort, envp) == 0)
-		{
-			ft_lstadd_back((void *)&sort, ft_lstnew((void *)ft_strdup(var)));
-			if (char_check(var, '=') != -1)
-				ft_lstadd_back((void *)&envp,
-					ft_lstnew((void *)ft_strdup(var)));
-		}
-	}
-}
-
-void	ft_env(t_lista *lst, char *is_not_null)
-{
-	t_lista	*current;
-
-	if (is_not_null)
-		g_gl.error_code = COMMAND_NOT_FOUND;
-	current = lst;
-	while (current != NULL && !g_gl.error_code)
-	{
-		printf("%s\n", (char *)current->content);
-		current = current->next;
-	}
 }
